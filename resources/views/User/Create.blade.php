8a3e054103dead131e6ae7104353a56eea052a50
AchmadPutraA12 <ahmad.arifki11@gmail.com>
2024-12-02 11:44:01 +0700

    sex bebas

--- resources/views/User/Create.blade.php
+++ resources/views/User/Create.blade.php
@@ -1,115 +1,132 @@
 @extends('Layouts.Main')
 @section('title', 'Kelembagaan - Tambah User')
 @section('content')
-<div class="main-content">
+    <div class="main-content">
 
-    <div class="page-content">
-        <div class="container-fluid">
+        <div class="page-content">
+            <div class="container-fluid">
 
-            <!-- start page title -->
-            <div class="row">
-                <div class="col-12">
-                    <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 class="mb-sm-0">Form Tambah User</h4>
+                <!-- start page title -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                            <h4 class="mb-sm-0">Form Tambah User</h4>
 
-                        <div class="page-title-right">
-                            <ol class="breadcrumb m-0">
-                                <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">Dashboard</a></li>
-                                <li class="breadcrumb-item"><a href="{{ route('user.index') }}">User</a></li>
-                                <li class="breadcrumb-item active">Tambah</li>
-                            </ol>
+                            <div class="page-title-right">
+                                <ol class="breadcrumb m-0">
+                                    <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">Dashboard</a></li>
+                                    <li class="breadcrumb-item"><a href="{{ route('user.index') }}">User</a></li>
+                                    <li class="breadcrumb-item active">Tambah</li>
+                                </ol>
+                            </div>
+
                         </div>
-
                     </div>
                 </div>
+                <!-- end page title -->
+
+                <form action="{{ route('user.store') }}" method="POST" class="row g-3" enctype="multipart/form-data">
+                    @csrf
+                    <div class="col-md-6">
+                        <label for="validationCustom01" class="required-label">Nama</label>
+                        <input type="text" class="form-control @error('name') is-invalid @enderror"
+                            id="validationCustom01" name="name" value="{{ old('name') }}" required>
+                        @error('name')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="col-md-6">
+                        <label for="validationCustom01" class="required-label">Email</label>
+                        <input type="email" class="form-control @error('email') is-invalid @enderror"
+                            id="validationCustom01" name="email" value="{{ old('email') }}" required>
+                        @error('email')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="col-md-6">
+                        <label for="validationCustom01" class="required-label">Password</label>
+                        <div class="input-group">
+                            <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                id="passwordInput" name="password" required>
+                            <span class="input-group-text" id="togglePassword" style="cursor: pointer;">
+                                <i class="bi bi-eye"></i>
+                            </span>
+                        </div>
+                        <small class="form-text text-muted">Minimal 10 Karakter pada password</small>
+                        @error('password')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="col-md-6">
+                        <label for="validationCustom01" class="required-label">Nomor Induk Pegawai</label>
+                        <input type="text" class="form-control @error('nip') is-invalid @enderror"
+                            id="validationCustom01" name="nip" value="{{ old('nip') }}">
+                        @error('nip')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="col-md-6">
+                        <label for="is_active" class="required-label">Status Aktif</label>
+                        <select class="form-control @error('is_active') is-invalid @enderror" id="is_active" data-choices
+                            name="is_active" required>
+                            <option value="">Pilih Status</option>
+                            <option value="1" {{ old('is_active') == '1' ? 'selected' : '' }}>Aktif</option>
+                            <option value="0" {{ old('is_active') == '0' ? 'selected' : '' }}>Tidak Aktif</option>
+                        </select>
+                        @error('is_active')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="col-md-6">
+                        <label for="RolesLabel" class="required-label">Role User</label>
+                        <select class="form-control @error('roles') is-invalid @enderror" id="roles" name="roles[]"
+                            multiple required>
+                            @foreach ($roles as $role)
+                                <option value="{{ $role }}"
+                                    {{ collect(old('roles'))->contains($role) ? 'selected' : '' }}>{{ $role }}
+                                </option>
+                            @endforeach
+                        </select>
+                        @error('roles')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+
+                    <div class="col-md-6">
+                        <label for="OrganizationLabel" class="required-label">Organisasi User</label>
+                        <select class="form-control @error('id_organization') is-invalid @enderror" id="id_organizations"
+                            name="id_organization" required>
+                            @foreach ($organization as $item)
+                                <option value="{{ $item->id }}"
+                                    {{ old('id_organization') == $item->id ? 'selected' : '' }}>
+                                    {{ $item->organisasi_nama }}</option>
+                            @endforeach
+                        </select>
+                        @error('id_organization')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="col-12 d-flex justify-content-end">
+                        <button type="button" class="btn btn-danger mb-1 me-1" onclick="window.history.back();">Go
+                            Back</button>
+                        <button class="btn btn-primary mb-1 me-1" type="submit">Submit form</button>
+                    </div>
+                </form>
+
+
             </div>
-            <!-- end page title -->
-
-            <form action="{{ route('user.store') }}" method="POST" class="row g-3" enctype="multipart/form-data">
-                @csrf
-                <div class="col-md-6">
-                    <label for="validationCustom01" class="required-label">Nama</label>
-                    <input type="text" class="form-control @error('name') is-invalid @enderror" id="validationCustom01" name="name" value="{{ old('name') }}" required>
-                    @error('name')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-
-                <div class="col-md-6">
-                    <label for="validationCustom01" class="required-label">Email</label>
-                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="validationCustom01" name="email" value="{{ old('email') }}" required>
-                    @error('email')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-
-                <div class="col-md-6">
-                    <label for="validationCustom01" class="required-label">Password</label>
-                    <input type="password" class="form-control @error('password') is-invalid @enderror" id="validationCustom01" name="password" required>
-                    <small class="form-text text-muted">Minimal 10 Karakter pada password</small>
-                    @error('password')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-
-                <div class="col-md-6">
-                    <label for="validationCustom01" class="required-label">Nomor Induk Pegawai</label>
-                    <input type="text" class="form-control @error('nip') is-invalid @enderror" id="validationCustom01" name="nip" value="{{ old('nip') }}">
-                    @error('nip')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-
-                <div class="col-md-6">
-                    <label for="is_active" class="required-label">Status Aktif</label>
-                    <select class="form-control @error('is_active') is-invalid @enderror" id="is_active" data-choices name="is_active" required>
-                        <option value="">Pilih Status</option>
-                        <option value="1" {{ old('is_active') == '1' ? 'selected' : '' }}>Aktif</option>
-                        <option value="0" {{ old('is_active') == '0' ? 'selected' : '' }}>Tidak Aktif</option>
-                    </select>
-                    @error('is_active')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-
-                <div class="col-md-6">
-                    <label for="RolesLabel" class="required-label">Role User</label>
-                    <select class="form-control @error('roles') is-invalid @enderror" id="roles" name="roles[]" multiple required>
-                        @foreach ($roles as $role)
-                        <option value="{{ $role }}" {{ (collect(old('roles'))->contains($role)) ? 'selected' : '' }}>{{ $role }}</option>
-                        @endforeach
-                    </select>
-                    @error('roles')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-
-
-                <div class="col-md-6">
-                    <label for="OrganizationLabel" class="required-label">Organisasi User</label>
-                    <select class="form-control @error('id_organization') is-invalid @enderror" id="id_organizations" name="id_organization" required>
-                        @foreach ($organization as $item)
-                        <option value="{{ $item->id }}" {{ old('id_organization') == $item->id ? 'selected' : '' }}>{{ $item->organisasi_nama }}</option>
-                        @endforeach
-                    </select>
-                    @error('id_organization')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-
-                <div class="col-12 d-flex justify-content-end">
-                    <button type="button" class="btn btn-danger me-1 mb-1" onclick="window.history.back();">Go Back</button>
-                    <button class="btn btn-primary me-1 mb-1" type="submit">Submit form</button>
-                </div>
-            </form>
-
-
+            <!-- container-fluid -->
         </div>
-        <!-- container-fluid -->
-    </div>
-    <!-- End Page-content -->
+        <!-- End Page-content -->
 
-    {{-- <footer class="footer">
+        {{-- <footer class="footer">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-sm-6">
@@ -125,34 +142,45 @@
             </div>
         </div>
     </footer> --}}
-</div>
+    </div>
 @endsection
 
 @section('js')
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        const element = document.getElementById('roles');
-        const choices = new Choices(element, {
-            removeItemButton: true,
-            placeholder: true,
-            placeholderValue: 'Pilih Role...',
-            maxItemCount: -1,
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const togglePassword = document.getElementById('togglePassword');
+            const passwordInput = document.getElementById('passwordInput');
+            togglePassword.addEventListener('click', function() {
+                const isPassword = passwordInput.type === 'password';
+                passwordInput.type = isPassword ? 'text' : 'password';
+                this.innerHTML = isPassword ? '<i class="bi bi-eye-slash"></i>' :
+                    '<i class="bi bi-eye"></i>';
+            });
         });
-    });
-    document.addEventListener('DOMContentLoaded', function() {
-        const element = document.getElementById('id_organizations');
-        const choices = new Choices(element, {
-            removeItemButton: false,
-            placeholder: true,
-            placeholderValue: 'Organisasi',
-            maxItemCount: 1, 
-            searchResultLimit: 5,
-            searchFields: ['label'],
-            fuseOptions: {
-                threshold: 0.3,
-                minMatchCharLength: 2,
-            }
+    </script>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const rolesElement = document.getElementById('roles');
+            new Choices(rolesElement, {
+                removeItemButton: true,
+                placeholder: true,
+                placeholderValue: 'Pilih Role...',
+                maxItemCount: -1,
+            });
+
+            const organizationsElement = document.getElementById('id_organizations');
+            new Choices(organizationsElement, {
+                removeItemButton: false,
+                placeholder: true,
+                placeholderValue: 'Organisasi',
+                searchResultLimit: -1,
+                searchFields: ['label'],
+                fuseOptions: {
+                    threshold: 0.3,
+                    minMatchCharLength: 2,
+                },
+            });
         });
-    }); 
-</script>
+    </script>
 @endsection
